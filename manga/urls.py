5e8f3561493ccace7fb93219f3c12bfacf63a9e7
Kadys Muktar <kadysmuktar06@gmail.com>
2025-05-10 11:50:31 +0500

    removed a comma

--- manga/urls.py
+++ manga/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='manga/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('manga/<int:manga_id>/', views.manga_detail, name='manga_detail'),
-    path('chapter/<int:chapter_id>/', views.chapter_detail, name='chapter_detail'),
+    path('chapter/<int:chapter_id>/', views.chapter_detail, name='chapter_detail')
 ]
